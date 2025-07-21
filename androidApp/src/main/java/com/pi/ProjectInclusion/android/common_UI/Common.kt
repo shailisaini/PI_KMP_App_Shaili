14fea09c79d6b47cf898e3353d9b169a361b1e9f
shailisaini <shaili.saini@aurosociety.org>
2025-07-21 10:05:26 +0530

    edit profile

--- androidApp/src/main/java/com/pi/ProjectInclusion/android/common_UI/Common.kt
+++ androidApp/src/main/java/com/pi/ProjectInclusion/android/common_UI/Common.kt
@@ -100,8 +100,6 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
