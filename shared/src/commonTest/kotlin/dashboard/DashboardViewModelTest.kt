62f089010c0fc62ec293c33e3af9f778e49abfa2
shailisaini <shaili.saini@aurosociety.org>
2025-09-05 09:27:23 +0530

    School Change request done!

---
+++ shared/src/commonTest/kotlin/dashboard/DashboardViewModelTest.kt
@@ -0,0 +1,132 @@
+package dashboard
+
+import authentication.FakeAuthenticationRepository
+import FakeConnectivityObserver
+import FakeLocalDataSource
+import com.pi.ProjectInclusion.data.model.authenticationModel.request.LoginRequest
+import com.pi.ProjectInclusion.data.model.profileModel.ProfileNameChangeRequest
+import com.pi.ProjectInclusion.domain.useCases.AuthenticationUsesCases
+import com.pi.ProjectInclusion.domain.useCases.DashboardUsesCases
+import com.pi.ProjectInclusion.ui.viewModel.DashboardViewModel
+import io.ktor.utils.io.core.toByteArray
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class LoginViewModelTest {
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    /**
+     * Fake for getAuthViewModel (instead of hitting real API)
+     */
+    private lateinit var fakeRepo: FakeDashboardRepository
+    private val testDispatcher = StandardTestDispatcher()
+
+    @BeforeTest
+    fun setup() {
+
+        // Set Main dispatcher for viewModelScope
+        Dispatchers.setMain(testDispatcher)
+        fakeRepo = FakeDashboardRepository()
+        val fakeAuthUseCases = DashboardUsesCases(fakeRepo)
+        val fakeLocalData = FakeLocalDataSource()
+        val fakeConnectivity = FakeConnectivityObserver()
+
+        dashboardViewModel = DashboardViewModel(
+            fakeAuthUseCases,
+            fakeLocalData,
+            fakeConnectivity
+        )
+    }
+
+    @Test
+    fun profile_change_request_emits_success() = runTest {
+        fakeRepo.shouldSucceed = true
+
+        val request = ProfileNameChangeRequest("newName") // adapt to your model
+
+        // Act
+        dashboardViewModel.getProfileChangeRequest(
+            request,
+            "fake_token",
+            docPic = "fakeBytes".toByteArray(),
+            fileName = "doc.png"
+        )
+
+        // advance dispatcher so launched coroutines run
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        // Assert
+        val state = dashboardViewModel.getChangeRequest.first { !it.isLoading }
+        assertNotNull(state.success)
+        assertEquals("Change request successful", state.success.message) // adapt message
+    }
+
+    @Test
+    fun profile_change_request_emits_error() = runTest {
+        // Arrange
+        fakeRepo.shouldSucceed = false
+        val request = ProfileNameChangeRequest("newName")
+
+        // Act
+        dashboardViewModel.getProfileChangeRequest(
+            request,
+            "fake_token"
+        )
+
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        // Assert
+        val state = dashboardViewModel.getChangeRequest.first { !it.isLoading }
+        assertNotNull(state.error)
+        assertEquals("Request Not sent!", state.error) // adapt message
+    }
+
+    @Test
+    fun track_change_request_emits_success() = runTest {
+        // Arrange
+        fakeRepo.shouldSucceed = true
+
+        // Act
+        dashboardViewModel.getTrackChangeRequest(
+            strToken = "fake_token",
+            requestTypeId = "123"
+        )
+
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        // Assert
+        val state = dashboardViewModel.getTrackRequest.first { !it.isLoading }
+        assertNotNull(state.success)
+        assertEquals("Track request successful", state.success.message) // adapt to your fake response
+    }
+
+    @Test
+    fun track_change_request_emits_error() = runTest {
+        // Arrange
+        fakeRepo.shouldSucceed = false
+
+        // Act
+        dashboardViewModel.getTrackChangeRequest(
+            strToken = "fake_token",
+            requestTypeId = "123"
+        )
+
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        // Assert
+        val state = dashboardViewModel.getTrackRequest.first { !it.isLoading }
+        assertNotNull(state.error)
+        assertEquals("Request Not found!", state.error) // adapt to your fake response
+    }
+
+}
---
+++ shared/src/commonTest/kotlin/dashboard/FakeDashboardRepository.kt
@@ -0,0 +1,100 @@
+package dashboard
+
+import com.pi.ProjectInclusion.data.model.authenticationModel.request.CertificateRequest
+import com.pi.ProjectInclusion.data.model.authenticationModel.response.CategoryListResponse
+import com.pi.ProjectInclusion.data.model.authenticationModel.response.CertificateListResponse
+import com.pi.ProjectInclusion.data.model.authenticationModel.response.FAQsListResponse
+import com.pi.ProjectInclusion.data.model.authenticationModel.response.SubCategoryByCategoryIdResponse
+import com.pi.ProjectInclusion.data.model.authenticationModel.response.SubCategoryListResponse
+import com.pi.ProjectInclusion.data.model.authenticationModel.response.TokenResponse
+import com.pi.ProjectInclusion.data.model.authenticationModel.response.ZoomMeetingListResponse
+import com.pi.ProjectInclusion.data.model.authenticationModel.response.ZoomMeetingTokenResponse
+import com.pi.ProjectInclusion.data.model.authenticationModel.response.ZoomMeetingsJoinResponse
+import com.pi.ProjectInclusion.data.model.profileModel.ProfileNameChangeRequest
+import com.pi.ProjectInclusion.data.model.profileModel.response.ChangeRequestResponse
+import com.pi.ProjectInclusion.data.model.profileModel.response.TrackRequestResponse
+import com.pi.ProjectInclusion.data.model.profileModel.response.TrackRequestResponse.RequestResponse
+import com.pi.ProjectInclusion.domain.repository.DashboardRepository
+
+class FakeDashboardRepository : DashboardRepository {
+    var shouldSucceed = true
+    override suspend fun getLMSUserCertificateRepo(
+        certificateRequest: CertificateRequest,
+        strToken: String
+    ): CertificateListResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getChangeRequestRepo(
+        certificateRequest: ProfileNameChangeRequest,
+        strToken: String,
+        profilePic: ByteArray?,
+        fileName: String?
+    ): ChangeRequestResponse {
+
+        return if (shouldSucceed) {
+            ChangeRequestResponse(status = true, message = "Change request successful", response = "", error ="")
+        } else {
+            throw Exception("Request Not sent!")
+        }
+    }
+
+    override suspend fun TrackRequestResponseRepo(
+        strToken: String,
+        requestTypeId: String
+    ): TrackRequestResponse {
+        return if (shouldSucceed) {
+            TrackRequestResponse(status = true, message = "Track request successful",
+                response = RequestResponse(true,"Already Requested"), error ="")
+        } else {
+            throw Exception("Request Not found!")
+        }
+    }
+
+    override suspend fun getAllCategoryRepo(): List<CategoryListResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllSubCategoryRepo(): List<SubCategoryListResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllSubCategoryByCategoryIdRepo(categoryId: Int): SubCategoryByCategoryIdResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllFAQsRepo(
+        strKeyword: String,
+        userTypeId: String,
+        categoryId: String,
+        subCategoryId: String,
+        userId: String,
+        languageId: String
+    ): FAQsListResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getRefreshTokenRepo(): ZoomMeetingTokenResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getZoomMeetingsActualTokenRepo(
+        strAuthKey: String,
+        strGrantType: String,
+        strRefreshToken: String
+    ): TokenResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllZoomMeetingsRepo(tokenKey: String): ZoomMeetingListResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getJoinZoomMeetingsRepo(
+        tokenKey: String,
+        meetingId: Long
+    ): ZoomMeetingsJoinResponse {
+        TODO("Not yet implemented")
+    }
+
+}
